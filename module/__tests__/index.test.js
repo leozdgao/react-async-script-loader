92020a09e7b1f38f73b72f7059243fbbe9771c9c
leozdgao <gzdleo0816@outlook.com>
2016-03-24 22:37:31 +0800

    init test folder

---
+++ module/__tests__/index.test.js
@@ -0,0 +1,9 @@
+jest.unmock('../../module')
+
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import AsyncScriptLoader from '../../module'
+
+describe('AsyncScriptLoader', _ => {
+  
+})
