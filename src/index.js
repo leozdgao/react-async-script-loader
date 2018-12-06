c738683d8811ad0e78b39f25627bc78649f6ea42
Andrew Lowther <Andrew.Lowther@gentinguk.com>
2018-12-06 10:59:44 +0000

    Core: Updated all internal referenced to the most modern versions
    Detailed:
    The modules in use on this project were seriously out of date, and
    represented a potential security risk on websites currently using it.
    
    I have updated all modules to their most modern versions possible,
    and fixed tests within the application, alongside the build process
    to ensure security is maintained.
    
    Signed-off-by: Andrew Lowther <Andrew.Lowther@gentinguk.com>

--- src/index.js
+++ src/index.js
@@ -1,131 +1,131 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import hoistStatics from 'hoist-non-react-statics'
-import { newScript, series, noop } from './utils'
+import {newScript, series, noop} from './utils'
 
 const loadedScript = []
 const pendingScripts = {}
 let failedScript = []
 
 export function startLoadingScripts(scripts, onComplete = noop) {
-  // sequence load
-  const loadNewScript = (script) => {
-    const src = typeof script === 'object' ? script.src : script
-    if (loadedScript.indexOf(src) < 0) {
-      return taskComplete => {
-        const callbacks = pendingScripts[src] || []
-        callbacks.push(taskComplete)
-        pendingScripts[src] = callbacks
-        if (callbacks.length === 1) {
-          return newScript(script)(err => {
-            pendingScripts[src].forEach(cb => cb(err, src))
-            delete pendingScripts[src]
-          })
+    // sequence load
+    const loadNewScript = (script) => {
+        const src = typeof script === 'object' ? script.src : script
+        if (loadedScript.indexOf(src) < 0) {
+            return taskComplete => {
+                const callbacks = pendingScripts[src] || []
+                callbacks.push(taskComplete)
+                pendingScripts[src] = callbacks
+                if (callbacks.length === 1) {
+                    return newScript(script)(err => {
+                        pendingScripts[src].forEach(cb => cb(err, src))
+                        delete pendingScripts[src]
+                    })
+                }
+            }
         }
-      }
     }
-  }
-  const tasks = scripts.map(src => {
-    if (Array.isArray(src)) {
-      return src.map(loadNewScript)
-    }
-    else return loadNewScript(src)
-  })
+    const tasks = scripts.map(src => {
+        if (Array.isArray(src)) {
+            return src.map(loadNewScript)
+        }
+        else return loadNewScript(src)
+    })
 
-  series(...tasks)((err, src) => {
-    if (err) {
-      failedScript.push(src)
-    }
-    else {
-      if (Array.isArray(src)) {
-        src.forEach(addCache)
-      }
-      else addCache(src)
-    }
-  })(err => {
-    removeFailedScript()
-    onComplete(err)
-  })
+    series(...tasks)((err, src) => {
+        if (err) {
+            failedScript.push(src)
+        }
+        else {
+            if (Array.isArray(src)) {
+                src.forEach(addCache)
+            }
+            else addCache(src)
+        }
+    })(err => {
+        removeFailedScript()
+        onComplete(err)
+    })
 }
 
 const addCache = (entry) => {
-  if (loadedScript.indexOf(entry) < 0) {
-    loadedScript.push(entry)
-  }
+    if (loadedScript.indexOf(entry) < 0) {
+        loadedScript.push(entry)
+    }
 }
 
 const removeFailedScript = () => {
-  if (failedScript.length > 0) {
-    failedScript.forEach((script) => {
-      const node = document.querySelector(`script[src='${script}']`)
-      if (node != null) {
-        node.parentNode.removeChild(node)
-      }
-    })
+    if (failedScript.length > 0) {
+        failedScript.forEach((script) => {
+            const node = document.querySelector(`script[src='${script}']`)
+            if (node != null) {
+                node.parentNode.removeChild(node)
+            }
+        })
 
-    failedScript = []
-  }
+        failedScript = []
+    }
 }
 
 const scriptLoader = (...scripts) => (WrappedComponent) => {
-  class ScriptLoader extends Component {
-    static propTypes = {
-      onScriptLoaded: PropTypes.func
-    }
+    class ScriptLoader extends Component {
+        constructor(props, context) {
+            super(props, context)
 
-    static defaultProps = {
-      onScriptLoaded: noop
-    }
-
-    constructor (props, context) {
-      super(props, context)
-
-      this.state = {
-        isScriptLoaded: false,
-        isScriptLoadSucceed: false
-      }
-
-      this._isMounted = false;
-    }
-
-    componentDidMount () {
-      this._isMounted = true;
-      startLoadingScripts(scripts, err => {
-        if(this._isMounted) {
-          this.setState({
-            isScriptLoaded: true,
-            isScriptLoadSucceed: !err
-          }, () => {
-            if (!err) {
-              this.props.onScriptLoaded()
+            this.state = {
+                isScriptLoaded: false,
+                isScriptLoadSucceed: false
             }
-          })
+
+            this._isMounted = false;
         }
-      })
+
+        componentDidMount() {
+            this._isMounted = true;
+            startLoadingScripts(scripts, err => {
+                if (this._isMounted) {
+                    this.setState({
+                        isScriptLoaded: true,
+                        isScriptLoadSucceed: !err
+                    }, () => {
+                        if (!err) {
+                            this.props.onScriptLoaded()
+                        }
+                    })
+                }
+            })
+        }
+
+        componentWillUnmount() {
+            this._isMounted = false;
+        }
+
+        getWrappedInstance() {
+            return this.refs.wrappedInstance;
+        }
+
+        render() {
+            const props = {
+                ...this.props,
+                ...this.state,
+                ref: 'wrappedInstance'
+            }
+
+            return (
+                <WrappedComponent {...props} />
+            )
+        }
     }
 
-    componentWillUnmount () {
-      this._isMounted = false;
-    }
-
-    getWrappedInstance () {
-      return this.refs.wrappedInstance;
-    }
-
-    render () {
-      const props = {
-        ...this.props,
-        ...this.state,
-        ref: 'wrappedInstance'
-      }
-
-      return (
-        <WrappedComponent {...props} />
-      )
-    }
-  }
-
-  return hoistStatics(ScriptLoader, WrappedComponent)
+    return hoistStatics(ScriptLoader, WrappedComponent)
 }
+
+scriptLoader.defaultProps = {
+    onScriptLoaded: noop
+};
+
+scriptLoader.propTypes = {
+    onScriptLoaded: PropTypes.func
+};
 
 export default scriptLoader
